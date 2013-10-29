882cecb905dea24f75b1e952005df00be4f15840
James Absalon <james.absalon@rackspace.com>
2013-10-29 15:14:16 +0000

    Real initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# cloud workload initialization stuffs
---
+++ workload.py
@@ -0,0 +1,34 @@
+class Workload(object):
+    """
+    Class that handles a cloud workload.  A workload should consist
+    of necessary tools to install, run, and parse output.
+
+    Configuration should go in self.config
+    Data collected during execution should go into self.data
+    """
+
+    def __init__(self):
+        """Nothing for now"""
+        self._config = self._config()
+        self._data = {}
+        pass
+
+    def _config(self):
+        """
+        Should load any necessary configuration for the workload.
+        Possibilities include webheads, db server addresses, number of
+        clients, etc.
+        """
+        return {}
+
+    def run(self):
+        """Runs the workload"""
+        return False
+
+    def view(self):
+        """
+        Should return a string view of this workload.  The string should be
+        valid html that can be dumped with other html.
+
+        :returns: String html representation of workload output
+        """
